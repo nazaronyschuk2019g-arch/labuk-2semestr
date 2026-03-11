72e42bc2f6d0362325eb4b61714bb587dc81406f
nazaronyschuk2019g-arch <nazaronyschuk2019.g@gmail.com>
2026-03-11 20:59:22 +0200

    Lab 4: Add models Category, Brand, Product and configure Admin

--- my_pages/admin.py
+++ my_pages/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # list_display вказує, які колонки виводити
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'price', 'created_at', 'updated_at')
