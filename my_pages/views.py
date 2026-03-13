0a4f0d2e12d7e51177e0d214f3ae63f673438929
nazaronyschuk2019g-arch <nazaronyschuk2019.g@gmail.com>
2026-03-13 08:32:28 +0200

    Lab 5: Add header, footer and display products from DB

--- my_pages/views.py
+++ my_pages/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render
+# Імпортуємо наші таблиці з бази даних
+from .models import Category, Product
+
 
 def home_view(request):
+    # Дістаємо ВСІ категорії та ВСІ товари з бази
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
     context = {
-        'title': 'Головна сторінка',
-        'content': 'Це головна сторінка. Обирай, куди йти далі:',
-        'is_main': True  # Цей прапорець скаже шаблону показати посилання на інші сторінки
+        'title': 'Cloud City - Головна',
+        'categories': categories,  # Передаємо категорії для меню
+        'products': products,  # Передаємо товари для вітрини
+        'is_main': True
     }
     return render(request, 'my_pages/base.html', context)
+
 
 def page1_view(request):
+    categories = Category.objects.all()
     context = {
-        'title': 'Перша сторінка',
-        'content': 'Ти знаходишся на першій сторінці!',
-        'is_main': False  # Цей прапорець скаже шаблону показати посилання НАЗАД на головну
+        'title': 'Про нас',
+        'categories': categories,
+        'is_main': False
     }
     return render(request, 'my_pages/base.html', context)
 
+
 def page2_view(request):
+    categories = Category.objects.all()
     context = {
-        'title': 'Друга сторінка',
-        'content': 'Ти знаходишся на другій сторінці!',
+        'title': 'Контакти',
+        'categories': categories,
         'is_main': False
     }
     return render(request, 'my_pages/base.html', context)
